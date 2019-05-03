6620665cd85e88c9d95f4b71ebcd440bbdfdd9a4
Garth Lilly <garff247@gmail.com>
2019-05-03 19:18:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,12 +11,23 @@ class Song
   end
 
   def self.create
-    @@all << self.new
+    new_song = self.new
+    new_song.save
+    new_song
   end
 
-  def self.new_by_name(name)
-    @name = name
-    @@all << name
+  def self.new_by_name(string)
+    new_song = self.new
+    new_song.name = string
+    new_song
   end
+
+  def self.create_by_name(string)
+    new_song = self.new
+    new_song.name = string
+    new_song.save
+    new_song
+  end
+
 
 end
